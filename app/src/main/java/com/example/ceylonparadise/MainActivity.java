db9600e8a47a620ccd8f22aebce4c785b8b62a63
ASUS <it20202286@my.sliit.lk>
2021-09-26 11:59:47 +0530

    AddCard

--- app/src/main/java/com/example/ceylonparadise/MainActivity.java
+++ app/src/main/java/com/example/ceylonparadise/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
+
                 Intent mainIntent = new Intent(MainActivity.this, Login.class);
                 MainActivity.this.startActivity(mainIntent);
                 MainActivity.this.finish();
